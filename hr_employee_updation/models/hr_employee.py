823a30ce9b56ef204a1af46b522cb0432cc7cb8f
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2019-07-04 13:09:20 +0300

    Add files via upload

--- hr_employee_updation/models/hr_employee.py
+++ hr_employee_updation/models/hr_employee.py
@@ -20,14 +20,9 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 ###################################################################################
-from datetime import datetime, timedelta, date
-
-# from calverter import Calverter
-from custom.addons.hr_employee_updation.models.calverter import Calverter
-from odoo import models, fields, api, _
-
-
-
+from datetime import datetime, timedelta
+from odoo import models, fields, _,api
+from odoo.exceptions import ValidationError
 GENDER_SELECTION = [('male', 'Male'),
                     ('female', 'Female'),
                     ('other', 'Other')]
@@ -66,9 +61,6 @@ class HrEmployeeFamilyInfo(models.Model):
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    arabic_name = fields.Char(string="Arabic Name")
-
-
     def mail_reminder(self):
         """Sending expiry date notification for ID and Passport"""
 
@@ -101,18 +93,11 @@ class HrEmployee(models.Model):
                         'body_html': mail_content,
                         'email_to': i.work_email,
                     }
-
                     self.env['mail.mail'].sudo().create(main_content).send()
-
-
     personal_mobile = fields.Char(string='Mobile', related='address_home_id.mobile', store=True)
     joining_date = fields.Date(string='Joining Date')
-    id_expiry_date = fields.Date(string='ID Expiry Date', help='Expiry date of Identification ID')
-    id_expiry_date_hajri = fields.Char(string='Expiry Date Hajri',compute='_calculate_id_hajri', help='Expiry date of Identification ID')
-
-    passport_expiry_date = fields.Date(string='Passport Expiry Date', help='Expiry date of Passport ID')
-    passport_expiry_date_hajri = fields.Char(string='Expiry Date Hajri',compute='_calculate_passport_hajri', help='Expiry date of Passport ID')
-
+    id_expiry_date = fields.Date(string='Expiry Date', help='Expiry date of Identification ID')
+    passport_expiry_date = fields.Date(string='Expiry Date', help='Expiry date of Passport ID')
     id_attachment_id = fields.Many2many('ir.attachment', 'id_attachment_rel', 'id_ref', 'attach_ref',
                                         string="Attachment", help='You can attach the copy of your Id')
     passport_attachment_id = fields.Many2many('ir.attachment', 'passport_attachment_rel', 'passport_ref', 'attach_ref1',
@@ -120,32 +105,27 @@ class HrEmployee(models.Model):
                                               help='You can attach the copy of Passport')
     fam_ids = fields.One2many('hr.employee.family', 'employee_id', string='Family', help='Family Information')
     emergency_contacts = fields.One2many('hr.emergency.contact', 'employee_obj', string='Emergency Contact')
-
-    @api.depends('id_expiry_date')
-    def _calculate_id_hajri(self):
-        cal = Calverter()
-        if self.id_expiry_date:
-            d = self.id_expiry_date
-
-            jd = cal.gregorian_to_jd(d.year, d.month, d.day)
-            # print(jd)
-            # print(cal.jd_to_islamic(jd))
-
-            hj = cal.jd_to_islamic(jd)
-            # print(hj[0], "/", hj[1], "/", hj[2])
-            self.id_expiry_date_hajri =str( hj[2])+ "/"+ str(hj[1])+ "/"+str(hj[0])
-
-    @api.depends('passport_expiry_date')
-    def _calculate_passport_hajri(self):
-        cal = Calverter()
-        if self.passport_expiry_date:
-            d = self.passport_expiry_date
-
-            jd = cal.gregorian_to_jd(d.year, d.month, d.day)
-            # print(jd)
-            # print(cal.jd_to_islamic(jd))
-
-            hj = cal.jd_to_islamic(jd)
-            # print(hj[0], "/", hj[1], "/", hj[2])
-            self.passport_expiry_date_hajri = str(hj[2]) + "/" + str(hj[1])+ "/" + str(hj[0])
-
+    ticket_ids=fields.One2many('hr.employee.tickets','employee_id',string='Tickets',help='Tickets Information')
+class hr_employee_tickets(models.Model):
+    _name="hr.employee.tickets"
+    _description="Table For Employee Tickets data"
+    ticket_type=fields.Selection([('single','Single'),('family','Family')],string="Ticket Type")
+    ticket_reg_date=fields.Datetime(string="Date Of Register",default=datetime.now())
+    ticket_airline=fields.Char("Ticket Airline")
+    ticket_class=fields.Selection([('economy','Economy'),('first','First Class'),
+                                   ('business','Business Class'),('hospitality','Hospitality')],
+                                  default='economy',string="Ticket Class")
+    ticket_number=fields.Integer("Ticket Number")
+    @api.one
+    @api.constrains("ticket_number")
+    def _change_num(self):
+            if self.ticket_type=='single' and self.ticket_number>1:
+                raise ValidationError("Ticket Number Must Be 1")
+    ticket_price=fields.Float("Price For One Ticket")
+    ticket_cost=fields.Float("Total Cost For Tickets",store=True,readonly=True,compute="_calc_tickets_cost")
+    @api.one
+    @api.depends("ticket_price", "ticket_number")
+    def _calc_tickets_cost(self):
+        if self.ticket_number or self.ticket_price:
+            self.ticket_cost = self.ticket_price * self.ticket_number
+    employee_id=fields.Many2one('hr.employee',string="Employees")
